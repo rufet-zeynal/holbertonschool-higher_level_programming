9d3c4375f288e0cb763f69a0f95c9e45d915e9ab
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-11-27 18:51:31 +0000

    the integer stored in the variable number, followed by Battery street, followed by a new line.

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number} Battery street")
-number = 333
-print(f"{number} Battery street")
-number = -98
-print(f"{number} Battery street")
+number1=98
+print(f"{number1} Battery street")
+number2=333
+print(f"{number2} Battery street")
+number3=-98
+print(f"{number3} Battery street")
