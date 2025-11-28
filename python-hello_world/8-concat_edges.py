3bc8e602e71604852348d5472dcadcfef82258c5
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-11-28 14:51:04 +0000

    this source code to print object-oriented programming with Python, followed by a new line.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-# str is already defined in the template
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# Do not create new variables
+# Use the str variable only
 print(str[39:67] + str[106:112] + str[:6])
