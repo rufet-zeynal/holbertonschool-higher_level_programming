39cc8359b95accba260b852bff0238352eb311e3
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-02 11:58:59 +0000

    Import argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
     argv = sys.argv
-    count = len(argv)-1
+    count = len(argv) - 1
     if count == 0:
-        print('0 arguments.')
+        print("0 arguments.")
     else:
         print('{} argument{}'.format(count, "", if count == 1 else "s"))
         for i in range(1, count + 1):
