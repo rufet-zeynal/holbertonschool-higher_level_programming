96683a72d7a40038b4a43444c737330421819904
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-04 18:37:36 +0000

    Unique integers

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_list = []
+    summ = 0
     for i in my_list:
         if i not in new_list:
             new_list.append(i)
-    return new_list
+            summ +=i
+    return summ
