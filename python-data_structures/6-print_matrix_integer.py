d826d138ed28b7469360c00869d33c69062aa224
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-04 11:38:51 +0000

    Matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for idx, number in row:
+            if idx != 0:
+                print(" ", end="")
+            print("{:d}".format(number), end="")
+        print()
