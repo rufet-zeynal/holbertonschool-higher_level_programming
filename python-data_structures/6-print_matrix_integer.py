89e9b719d953935b8feaeb86ac44a2119a8ea532
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-04 11:40:09 +0000

    Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for idx, number in row:
+        for idx, number in enumarate(row):
             if idx != 0:
                 print(" ", end="")
             print("{:d}".format(number), end="")
