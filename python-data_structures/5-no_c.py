567361cb4fa3b759298f8d450e436ed538e4d424
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-04 11:16:12 +0000

    C removing

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,6 @@ def no_c(my_string):
     newstring=""
     for st in my_string:
         if st == 'c' or st == 'C':
-            return None
-        else:
-            newstring += str(st)
+            continue
+        newstring += st
     return newstring
