d28a7f1a8511daab534d3fd15280966fbf65e4b7
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-04 20:04:33 +0000

    Max value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     best_key = None
-    max_value = 0
+    max_value = float('-inf')
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
