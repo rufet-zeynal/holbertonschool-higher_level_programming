cf19ebdfc0ea7a45516a3e6e7d0580ecf2c4b2b8
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-12 14:07:15 +0000

    in-out 11

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student class module"""
+
+
+class Student:
+    """Defines a student with basic info"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation of the student
+        If attrs is a list of strings, only include those attributes
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__.copy()
+
