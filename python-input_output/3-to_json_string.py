f08d84e30417454a34469f4688be7a7a5497879c
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-12 13:47:23 +0000

    in-out 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a string to a text file and returns number of characters added"""
+
+
+def append_write(filename="", text=""):
+    """Appends text to a UTF-8 file and returns number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
