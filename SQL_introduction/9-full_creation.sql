df5aeda8493e9d0e139464ff83308c505237f7ba
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-19 12:42:00 +0000

    sql task

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 INSERT INTO second_table
 VALUES
-    (1, 'John', 10)
-    (2, 'Alex', 3)
-    (3, 'Bob', 14)
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
     (4, 'George', 8);
