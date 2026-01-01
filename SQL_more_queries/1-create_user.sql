34f8eda5c3d79e02d5972d2bb97f9d99e05eadf5
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-01 19:36:34 +0000

    SQL queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,3 @@
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Showing all previleges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
