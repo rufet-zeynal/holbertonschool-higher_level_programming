6e20efdde0e181e471e4d0db0b909f25960f3831
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-01 19:51:33 +0000

    Sql query

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant select
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
