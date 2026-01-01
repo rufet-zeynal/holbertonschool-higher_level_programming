7e04c58f90910af27732212666106f1b4570eb42
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-01 19:31:15 +0000

    SQL queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Creating MYSQL Server
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Showing all previleges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
