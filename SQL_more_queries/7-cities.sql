581750a937bf4f807cc4270cd370bc977bfb4a16
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-02 16:46:13 +0000

    query

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,7 +4,14 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa;
 -- Use database
 USE hbtn_0d_usa;
 
---Creating table 
+-- Table states
+CREATE TABLE IF NOT EXISTS states(
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY (id)
+);
+
+-- table cities
 CREATE TABLE IF NOT EXISTS cities(
 	id INT NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
