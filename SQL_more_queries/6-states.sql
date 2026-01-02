ff8be352b645ec942b6cc1982efd74bea1b41ac1
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-02 16:08:27 +0000

    query

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,12 @@
 -- Creating database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use database
+USE hbtn_0d_usa;
+
 -- Create table
 CREATE TABLE IF NOT EXISTS states(
-	id INT NOT NULL AUTO_CREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
 );
