a1c26705213be39ce10bd43b36be488f87352699
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-03 19:25:08 +0000

    query

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- TASK
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL 
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
