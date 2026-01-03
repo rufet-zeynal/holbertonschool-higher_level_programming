06b4a9cfa4134c4a0bc1e008e511a10d07c5fe9c
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-03 13:38:07 +0000

    query

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- all cities contained in the database hbtn_0d_usa
 SELECT id, name, states.name
 FROM cities
-JOIN states ON states.id = state_id
+JOIN states ON states.id = cities.state_id
 ORDER BY cities.id ASC
